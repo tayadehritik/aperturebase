dcfecd53edfcda4bedc3d05ec98f55f1ffff7823
braceritchie <braceritchie@protonmail.com>
2018-05-13 13:20:46 +0530

    new hosts added

--- aperturebase/settings.py
+++ aperturebase/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k!u-0=l$(=*^_d$baqs=f!n5@-ivkrnat+0@z*s5$#aij^b#_='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.232.53.55']
+ALLOWED_HOSTS = ['13.232.53.55','127.0.0.1']
 
 
 
