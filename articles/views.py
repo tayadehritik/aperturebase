868eac35816a41e2bd7244f5e48a95a036c9d777
braceritchie <braceritchie@protonmail.com>
2018-05-14 12:30:35 +0530

    url capturing

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Articles
 # Create your views here.
 def article_list(request):
     article = Articles.objects.all().order_by('date')
     return render(request, 'articles/article_list.html',{'article':article})
+
+def article_detail(request,slug):
+    return HttpResponse(slug)
